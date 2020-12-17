912037e3d99d8648b738a5f60da494f648826149
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2020-12-17 12:52:44 -0300

    Changes on the API
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,9 +1,9 @@
 resolvers += Resolver.bintrayRepo("oyvindberg", "converter")
 
-addSbtPlugin("org.scalablytyped.converter" % "sbt-converter" % "1.0.0-beta29")
+addSbtPlugin("org.scalablytyped.converter" % "sbt-converter" % "1.0.0-beta29.1")
 
 addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.2")
 
 addSbtPlugin("edu.gemini" % "sbt-lucuma" % "0.3.3")
 
-addSbtPlugin("com.geirsson" % "sbt-ci-release" % "1.5.4")
+addSbtPlugin("com.geirsson" % "sbt-ci-release" % "1.5.5")
--- src/main/scala/lucuma/mousetrap/package.scala
+++ src/main/scala/lucuma/mousetrap/package.scala
@@ -5,25 +5,35 @@ import japgolly.scalajs.react.vdom.html_<^._
 import react.common._
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
+import scala.scalajs.js.JSConverters._
 import lucuma.mt.mousetrap.mod.ExtendedKeyboardEvent
 
 package object mousetrap {
 
   val Mousetrap = lucuma.mt.mousetrap.mod.^
-  type CallbackFn = js.Function2[ /* e */ ExtendedKeyboardEvent, /* combo */ String, Unit]
+  type CallbackFn = js.Function2[ /* e */ ExtendedKeyboardEvent, /* combo */ String, Boolean]
 
-  implicit class CallbackOps(cb: Callback) {
+  implicit class CallbackOps(cb: CallbackTo[Boolean]) {
     def toCBF: CallbackFn = (_: ExtendedKeyboardEvent, _: String) => cb.runNow()
   }
 }
 
 package mousetrap {
+  final case class KeyBinding(keys: List[String], action: CallbackTo[Boolean])
+
+  object KeyBinding {
+    def apply(key: String, action:       => Callback): KeyBinding =
+      KeyBinding(List(key), action.map(_ => true))
+
+    def apply(key: List[String], action: => Callback): KeyBinding =
+      KeyBinding(key, action.map(_ => true))
+  }
 
   /**
    * Wraps a component in some keyboard bindings
    */
   final case class ReactMousetrap(
-    bindings: Map[String, Callback]
+    bindings: List[KeyBinding]
   ) extends ReactPropsWithChildren[ReactMousetrap](ReactMousetrap.component) {
     @inline def render: Seq[CtorType.ChildArg] => VdomElement =
       ReactMousetrap.component(this)
@@ -37,10 +47,10 @@ package mousetrap {
       .stateless
       .render_C(c => c)
       .componentDidMount { $ =>
-        Callback($.props.bindings.foreach { case (k, cb) => Mousetrap.bind(k, cb.toCBF) })
+        Callback($.props.bindings.foreach(b => Mousetrap.bind(b.keys.toJSArray, b.action.toCBF)))
       }
       .componentWillUnmount { $ =>
-        Callback($.props.bindings.foreach { case (k, _) => Mousetrap.unbind(k) })
+        Callback($.props.bindings.foreach(b => Mousetrap.unbind(b.keys.toJSArray)))
       }
       .build
   }
